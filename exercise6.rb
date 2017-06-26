2b3877ce88c878747305f5dd8afeaab93bebe1cb
AlB <albert.burc@gmail.com>
2017-06-26 16:16:29 -0400

    finished exercise6

--- exercise6.rb
+++ exercise6.rb
@@ -1,16 +1,26 @@
 
 question = "Would you like to walk or run?"
 distance = 0
+energy = 50
 
 loop do
   puts question
 input = gets.chomp
  if input == "walk"
-  distance +=1
+  distance += 1
+  energy += 5
   puts "Distance from home is #{distance}km."
+  puts "#{energy} energy remaining."
  elsif input == "run"
-  distance +=5
-  puts "Distance from home is #{distance}km."
+   if energy < 15
+     print "You are out of energy. Please walk to refill."
+   elsif energy >= 15
+
+    distance += 5
+    energy -= 15
+    puts "Distance from home is #{distance}km."
+    puts "#{energy} energy remaining."
+  end
 elsif input == "go home"
   break
  end
