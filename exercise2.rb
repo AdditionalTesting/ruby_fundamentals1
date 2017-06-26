fb2c3b68493163880f388e15d15f818f4321eb1c
AlB <albert.burc@gmail.com>
2017-06-26 13:36:06 -0400

    finished exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+#Question 1
+
+meal_price = 55
+tip = 1.2 * meal_price
+
+puts tip
+
+#Question 2
+
+puts "Hello" + 10.to_s
+
+#Question 3
+
+puts "The answer to this question is #{45628 * 7839}"
+
+#Question 4
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
